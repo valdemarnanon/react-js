cf5a626191ed823379391917df1ec1cf8bf3f181
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-11-16 15:44:01 -0300

    +

---
+++ listadetarefa/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    requireConfigFile: false,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "react-hooks"],
+  rules: {
+    "react/jsx-filename-extension": 0,
+  },
+};
